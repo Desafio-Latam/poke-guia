8d8758c824085a94e460a5515e9acb056850aaa3
kofrann <kofran.cl@gmail.com>
2022-04-07 19:14:01 -0400

    subida archivos

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="display-2 mb-3">{{ titulo }}</h1>
+        </div>
+        <div class="col-12">
+          <input
+            type="text"
+            class="form-control mb-3"
+            placeholder="Busca tu Pokemon"
+            v-model="pokemon"
+          />
+        </div>
+        <div class="col-12">
+          <button class="btn btn-success mb-4 w-50" @click="buscaPokemon">
+            Buscar Pokemon
+          </button>
+        </div>
+        <div class="col-12">
+          <h3>Nombre del Pokemon</h3>
+          <p>{{ nombrePokemon }}</p>
+        </div>
+        <div class="col-12">
+          <!--seteo src con : para dejarla reactiva-->
+          <img
+            :src="imagenPoke"
+            :alt="nombrePokemon"
+            width="200"
+            height="auto"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      pokemon: "pikachu", //poner al pikachu
+      //Colocamos la direccion de la api
+      URL_BASE: "https://pokeapi.co/api/v2/pokemon/",
+      pokemonData: [],
+    };
+  },
+  created() {
+    this.obtenerPokemon();
+  },
+  computed: {
+    titulo() {
+      //titulo es una vmodel reactiva y se llama desde el input
+      const titulo = "Listado Poke Api";
+      return titulo;
+    },
+    nombrePokemon() {
+      return this.pokemonData.name;
+    },
+    imagenPoke() {
+      return this.pokemonData.sprites.other.home; //poner direccion correcta
+    },
+  },
+  methods: {
+    async obtenerPokemon() {
+      try {
+        //aca con los basckytik  conectamos la direccion, y dejamos establecido
+        const req = await fetch(`${this.URL_BASE}${this.pokemon}`);
+        const data = await req.json();
+        this.pokemonData = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    buscaPokemon() {
+      this.obtenerPokemon();
+    },
   },
 };
 </script>
